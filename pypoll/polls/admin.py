d3dc3aafea4471e372962bb1c5228ddef0c45815
illite <abdallah.aden@gmail.com>
2025-05-27 04:12:48 -0400

    added admin functionality

--- pypoll/polls/admin.py
+++ pypoll/polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header= 'Python Poll'
+admin.site.site_title= 'Python Poll Admin area'
+admin.site.index_title= 'Welcome to PyPoll Admin area'
+
+
+class ChoiceInline(admin.TabularInline):
+    model= Choice
+    extra= 3
+
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields':['question_text']}),
+                 ('Date Information',{'fields':['pub_date'], 'classes':['collapse']}),]
+    inlines = [ChoiceInline]
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question,QuestionAdmin)
+
